fb254742526a60722d2a38b52cdb0bb6f365b79f
jcizel <jcizel@gmail.com>
2014-11-18 13:15:03 +0100

    minor changes

--- shell/upload.sh
+++ shell/upload.sh
@@ -1,1 +1,4 @@
-rsync -avr --relative --progress --stats --human-readable  /Users/jankocizel/Documents/Dropbox/Projects/PhD\ Thesis/R/./PACKAGES/WRDSHelpers wrds-cloud:/scratch/uvanl/.
+rsync -avr --relative --progress --stats --human-readable  /Users/jankocizel/Documents/Dropbox/Projects/PhD\ Thesis/R/./PACKAGES/WRDSHelpers wrds-cloud:~/.
+
+# TO BE EXECUTED ON REMOTE HOST:
+# ln -s ~/PACKAGES/WRDSHelpers/sasenvir/sasv9.cfg ~/.
